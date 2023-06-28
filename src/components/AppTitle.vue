71c398e63ea710ddf57eeafe52d99bcfe388a4a4
Matteo-Nesti <matteo.nesti@hotmail.com>
2023-06-28 15:50:17 +0200

    first commit

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    data() {
+        return {
+
+        };
+    },
+};
+</script>
+
+<template>
+    <h1>La mia Prima App con Vite</h1>
+</template>
+
+<style>
+h1 {
+    font-family: sans-serif;
+}
+</style>
